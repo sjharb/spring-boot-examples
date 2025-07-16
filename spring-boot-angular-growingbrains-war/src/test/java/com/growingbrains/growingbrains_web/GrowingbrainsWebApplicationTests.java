3727f266c070199b3d491412545d6f7015c4b52f
sjharb <zzmharbison@gmail.com>
2025-07-16 10:24:04 -0500

    Added war version of spring boot app since the other is jar deploy now.

---
+++ spring-boot-angular-growingbrains-war/src/test/java/com/growingbrains/growingbrains_web/GrowingbrainsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growingbrains.growingbrains_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowingbrainsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
